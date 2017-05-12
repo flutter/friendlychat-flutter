fbce7415ec9f75663ee2c5f41a456f629685ba79
Collin Jackson <jackson@google.com>
2017-05-12 15:45:12 -0700

    Update to match current codelab content

---
+++ full/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FirebaseAnalyticsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FirebaseAnalyticsPlugin"]];
+  [FirebaseAuthPlugin registerWithRegistrar:[registry registrarForPlugin:@"FirebaseAuthPlugin"]];
+  [FirebaseDatabasePlugin registerWithRegistrar:[registry registrarForPlugin:@"FirebaseDatabasePlugin"]];
+  [FirebaseStoragePlugin registerWithRegistrar:[registry registrarForPlugin:@"FirebaseStoragePlugin"]];
+  [GoogleSignInPlugin registerWithRegistrar:[registry registrarForPlugin:@"GoogleSignInPlugin"]];
+  [ImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"ImagePickerPlugin"]];
+}
+
+@end
